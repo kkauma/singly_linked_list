5cb929404adcad60bfa8ce5a910f62a4ad386ffe
Kyle Kauma <kylekauma@gmail.com>
2021-07-18 19:34:18 -0500

    add SSL file

---
+++ app.js
@@ -0,0 +1,190 @@
+/***************************/
+/*** Singly Linked List ***/
+/*************************/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+    this.tail = newTail;
+    this.tail.next = null;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
+  shift() {
+    if (!this.head) return undefined;
+    let currentHead = this.head;
+    this.head = currentHead.next;
+    this.length--;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return currentHead;
+  }
+  unshift(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let counter = 0;
+    let current = this.head;
+    while (counter !== index) {
+      current = current.next;
+      counter++;
+    }
+    return current;
+  }
+  set(index, val) {
+    let foundNode = this.get(index);
+    if (foundNode) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    let newNode = new Node(val);
+    let prev = this.get(index - 1);
+    let temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    let previousNode = this.get(index - 1);
+    let removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+  print() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
+}
+
+/**************/
+/*** Tests ***/
+/************/
+
+const list = new SinglyLinkedList();
+list.push("JavaScript");
+list.push("Python");
+list.push("React");
+list.push("Java");
+list.push("Philz Coffee");
+// console.log(list);
+list.insert(0, "Stripe");
+// list.remove(0);
+// list.remove(1);
+console.log(list.print());
+list.reverse();
+console.log(list.print());
+// console.log(list.insert(100, "Data"));
+// console.log(list);
+// console.log(list.get(0));
+// console.log(list.get(-1));
+// console.log(list.get(3));
+// list.set(4, "Starbucks");
+// console.log(list);
+// list.push("Hello");
+// // console.log(list);
+// list.push("there!");
+// list.push(99);
+// console.log(list);
+// list.pop();
+// console.log(list);
+// list.pop();
+// list.pop();
+// console.log(list);
+// list.push("JavaScript");
+// list.push("Python");
+// list.push("React");
+// console.log(list);
+// list.shift();
+// list.shift();
+// list.shift();
+// console.log(list);
+// list.unshift("Go Lang");
+// list.unshift("C++");
+// list.unshift("Java");
+// console.log(list);
+// console.log(list.head.next);
+// console.log(list.head.next.next);
+
+// const first = new Node("Hi");
+// first.next = new Node("there");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
+// console.log(Node);
+// console.log(first);
